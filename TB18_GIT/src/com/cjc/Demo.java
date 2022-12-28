1fd5486cef0ae39b4d4848ee32c60f081ef45982
Prburange <121481341+Prburange@users.noreply.github.com>
2022-12-28 13:04:50 +0530

    Update Demo.java

--- TB18_GIT/src/com/cjc/Demo.java
+++ TB18_GIT/src/com/cjc/Demo.java
@@ -5,7 +5,7 @@ public class Demo
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello GIT");
-		System.out.println("Welcome Pratiksha")
+		System.out.println("Welcome Pratiksha");
 		
 	}
 
